d37ffbe385dac19a89d57dfc9dd703bed8fcc1bb
Jürgen Ryannel <juergen@ryannel.org>
2022-05-11 15:56:44 +0200

    add version cmd

--- main.go
+++ main.go
@@ -6,8 +6,22 @@ import (
 	"os"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+type VersionInfo struct {
+	Version string `json:"version"`
+	Commit  string `json:"commit"`
+	Date    string `json:"date"`
+}
+
 func main() {
 	rootCmd := cmd.NewRootCommand()
+	rootCmd.Version = fmt.Sprintf("%s-%s-%s", version, commit, date)
+
 	err := rootCmd.Execute()
 	if err != nil {
 		fmt.Println(err)
