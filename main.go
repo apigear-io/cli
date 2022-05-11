57cc2f19860cec0693a497813d7cb7c0d143086b
Jürgen Ryannel <juergen@ryannel.org>
2022-05-11 09:44:59 +0200

    restored main

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"objectapi/cmd"
+	"os"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCommand()
+	err := rootCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+}
