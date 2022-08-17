6fc15285b3b50e162163352e49071938fa02a893
Jürgen Ryannel <juergen@ryannel.org>
2022-08-17 09:32:13 +0200

    wip: version info

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/apigear-io/cli/cmd"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	fmt.Printf("apigear-cli version %s, commit %s, built at %s\n", version, commit, date)
 	if cmd.Run() != 0 {
 		os.Exit(1)
 	}
