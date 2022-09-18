b92fa7941753edae9a69a34efe5de8d9b45ce037
Jürgen Ryannel <juergen@ryannel.org>
2022-09-18 16:36:41 +0200

    changed gitignore

---
+++ cmd/apigear/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/apigear-io/cli/pkg/cmd"
+	"github.com/apigear-io/cli/pkg/log"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+func main() {
+	log.Debugf("version: %s-%s-%s", version, commit, date)
+	if cmd.Run() != 0 {
+		os.Exit(1)
+	}
+}
