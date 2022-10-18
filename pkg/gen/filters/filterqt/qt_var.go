eb614a62d6bb5babdede8468de84bb7ea24b5d24
Jürgen Ryannel <juergen@ryannel.org>
2022-10-18 12:39:03 +0200

    remove unwanted dep

--- pkg/gen/filters/filterqt/qt_var.go
+++ pkg/gen/filters/filterqt/qt_var.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 
 	"github.com/apigear-io/cli/pkg/model"
-	"github.com/iancoleman/strcase"
+	"github.com/ettle/strcase"
 )
 
 func ToVarString(node *model.TypedNode) (string, error) {
 	if node == nil {
 		return "xxx", fmt.Errorf("ToVarString node is nil")
 	}
-	return strcase.ToLowerCamel(node.Name), nil
+	return strcase.ToCamel(node.Name), nil
 }
 
 func qtVar(node *model.TypedNode) (string, error) {
