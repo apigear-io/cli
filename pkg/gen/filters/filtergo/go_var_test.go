a60dc60cb92ddeb5b038e8e9fdadfa2cd2770bc6
Jürgen Ryannel <juergen.ryannel@icloud.com>
2022-11-21 17:42:48 +0100

    go filter support precision types

--- pkg/gen/filters/filtergo/go_var_test.go
+++ pkg/gen/filters/filtergo/go_var_test.go
@@ -19,11 +19,19 @@ func TestVar(t *testing.T) {
 
 		{"test", "Test1", "propBool", "propBool"},
 		{"test", "Test1", "propInt", "propInt"},
+		{"test", "Test1", "propInt32", "propInt32"},
+		{"test", "Test1", "propInt64", "propInt64"},
 		{"test", "Test1", "propFloat", "propFloat"},
+		{"test", "Test1", "propFloat32", "propFloat32"},
+		{"test", "Test1", "propFloat64", "propFloat64"},
 		{"test", "Test1", "propString", "propString"},
 		{"test", "Test1", "propBoolArray", "propBoolArray"},
 		{"test", "Test1", "propIntArray", "propIntArray"},
+		{"test", "Test1", "propInt32Array", "propInt32Array"},
+		{"test", "Test1", "propInt64Array", "propInt64Array"},
 		{"test", "Test1", "propFloatArray", "propFloatArray"},
+		{"test", "Test1", "propFloat32Array", "propFloat32Array"},
+		{"test", "Test1", "propFloat64Array", "propFloat64Array"},
 		{"test", "Test1", "propStringArray", "propStringArray"},
 	}
 	for _, sys := range syss {
