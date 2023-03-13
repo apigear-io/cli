dd287ec33c72ec5f4d221d0e10e2b5e29214c50c
dphan10 <dorota.phan@siili.com>
2023-03-13 16:23:12 +0100

    fix(qt_filters): qt_param filter now uses the prefix parameter

--- pkg/gen/filters/filterqt/qt_param_test.go
+++ pkg/gen/filters/filterqt/qt_param_test.go
@@ -71,3 +71,71 @@ func TestParamSymbols(t *testing.T) {
 		}
 	}
 }
+
+func TestParamWithPrefix_prefixIsIgnoredForBuiltInTypes(t *testing.T) {
+	table := []struct {
+		mn string
+		in string
+		pn string
+		rt string
+	}{
+		{"test", "Test1", "propBool", "bool propBool"},
+		{"test", "Test1", "propInt", "int propInt"},
+		{"test", "Test1", "propInt32", "qint32 propInt32"},
+		{"test", "Test1", "propInt64", "qint64 propInt64"},
+		{"test", "Test1", "propFloat", "qreal propFloat"},
+		{"test", "Test1", "propFloat32", "float propFloat32"},
+		{"test", "Test1", "propFloat64", "double propFloat64"},
+		{"test", "Test1", "propString", "const QString& propString"},
+		{"test", "Test1", "propBoolArray", "const QList<bool>& propBoolArray"},
+		{"test", "Test1", "propIntArray", "const QList<int>& propIntArray"},
+		{"test", "Test1", "propInt32Array", "const QList<qint32>& propInt32Array"},
+		{"test", "Test1", "propInt64Array", "const QList<qint64>& propInt64Array"},
+		{"test", "Test1", "propFloatArray", "const QList<qreal>& propFloatArray"},
+		{"test", "Test1", "propFloat32Array", "const QList<float>& propFloat32Array"},
+		{"test", "Test1", "propFloat64Array", "const QList<double>& propFloat64Array"},
+		{"test", "Test1", "propStringArray", "const QList<QString>& propStringArray"},
+	}
+	prefix := "my_prefix::"
+	syss := loadTestSystems(t)
+	for _, sys := range syss {
+		for _, tt := range table {
+			t.Run(tt.pn, func(t *testing.T) {
+				prop := sys.LookupProperty(tt.mn, tt.in, tt.pn)
+				assert.NotNil(t, prop)
+				r, err := qtParam(prefix, prop)
+				assert.NoError(t, err)
+				assert.Equal(t, tt.rt, r)
+			})
+		}
+	}
+}
+
+func TestParamSymbolsWithPrefix(t *testing.T) {
+	table := []struct {
+		mn string
+		in string
+		pn string
+		rt string
+	}{
+		{"test", "Test2", "propEnum", "my_prefix::Enum1::Enum1Enum propEnum"},
+		{"test", "Test2", "propStruct", "const my_prefix::Struct1& propStruct"},
+		{"test", "Test2", "propInterface", "my_prefix::Interface1 *propInterface"},
+		{"test", "Test2", "propEnumArray", "const QList<my_prefix::Enum1::Enum1Enum>& propEnumArray"},
+		{"test", "Test2", "propStructArray", "const QList<my_prefix::Struct1>& propStructArray"},
+		{"test", "Test2", "propInterfaceArray", "const QList<my_prefix::Interface1*>& propInterfaceArray"},
+	}
+	prefix := "my_prefix::"
+	syss := loadTestSystems(t)
+	for _, sys := range syss {
+		for _, tt := range table {
+			t.Run(tt.pn, func(t *testing.T) {
+				prop := sys.LookupProperty(tt.mn, tt.in, tt.pn)
+				assert.NotNil(t, prop)
+				r, err := qtParam(prefix, prop)
+				assert.NoError(t, err)
+				assert.Equal(t, tt.rt, r)
+			})
+		}
+	}
+}
