e3110f4bb6a54080b64a38a83b3313cb90650bce
Wolfgang Bremer <wbremer@apigear.io>
2023-06-02 12:52:36 +0200

    fix(js): remove camelcase from var name filter
    The strcase.ToCamel conversion broke compability to the params filter
    depending on the variable name.

--- pkg/gen/filters/filterjs/js_var.go
+++ pkg/gen/filters/filterjs/js_var.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 
 	"github.com/apigear-io/cli/pkg/model"
-	"github.com/ettle/strcase"
 )
 
 func ToVarString(node *model.TypedNode) (string, error) {
 	if node == nil {
 		return "xxx", fmt.Errorf("ToVarString node is nil")
 	}
-	return strcase.ToCamel(node.Name), nil
+	return node.Name, nil
 }
 
 func jsVar(node *model.TypedNode) (string, error) {
