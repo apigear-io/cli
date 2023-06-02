e4dcffe9f05fbd5546f98f95a9c8184200335f34
Wolfgang Bremer <wbremer@apigear.io>
2023-06-02 12:42:28 +0200

    test(cpp): extend tests for vars filter

--- pkg/gen/filters/filtercpp/cpp_vars_test.go
+++ pkg/gen/filters/filtercpp/cpp_vars_test.go
@@ -25,6 +25,9 @@ func TestVars(t *testing.T) {
 		{"test", "Test3", "opIntArray", "param1"},
 		{"test", "Test3", "opFloatArray", "param1"},
 		{"test", "Test3", "opStringArray", "param1"},
+		{"test", "Test3", "op_Bool", "param_Bool"},
+		{"test", "Test3", "op_bool", "param_bool"},
+		{"test", "Test3", "op_1", "param_1"},
 	}
 	syss := loadTestSystems(t)
 	for _, sys := range syss {
