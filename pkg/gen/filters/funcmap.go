488e641f31bd43238196f7ffa07b2186d1fd3238
Jürgen Ryannel <juergen@ryannel.org>
2024-03-12 09:56:20 +0100

    extract common filters

--- pkg/gen/filters/funcmap.go
+++ pkg/gen/filters/funcmap.go
@@ -12,54 +12,12 @@ import (
 	"github.com/apigear-io/cli/pkg/gen/filters/filterrs"
 	"github.com/apigear-io/cli/pkg/gen/filters/filterts"
 	"github.com/apigear-io/cli/pkg/gen/filters/filterue"
-	"github.com/apigear-io/cli/pkg/helper"
 )
 
 func PopulateFuncMap() template.FuncMap {
 	fm := make(template.FuncMap)
-	fm["snake"] = common.SnakeCaseLower
-	fm["Snake"] = common.SnakeTitleCase
-	fm["SNAKE"] = common.SnakeUpperCase
-	fm["camel"] = common.CamelLowerCase
-	fm["Camel"] = common.CamelTitleCase
-	fm["CAMEL"] = common.CamelUpperCase
-	fm["space"] = common.SpaceLowerCase
-	fm["Space"] = common.SpaceTitleCase
-	fm["SPACE"] = common.SpaceUpperCase
-	fm["dot"] = common.DotLowerCase
-	fm["Dot"] = common.DotTitleCase
-	fm["DOT"] = common.DotUpperCase
-	fm["kebap"] = common.KebabLowerCase
-	fm["Kebab"] = common.KebabTitleCase
-	fm["KEBAP"] = common.KebabUpperCase
-	fm["path"] = common.PathLowerCase
-	fm["Path"] = common.PathTitleCase
-	fm["PATH"] = common.PathUpperCase
-	fm["lower"] = common.LowerCase
-	fm["upper"] = common.UpperCase
-	fm["upper1"] = common.UpperFirst
-	fm["lower1"] = common.LowerFirst
-	fm["first"] = common.FirstCharLower
-	fm["First"] = common.FirstChar
-	fm["FIRST"] = common.FirstCharUpper
-	fm["join"] = common.Join
-	fm["split"] = common.Split
-	fm["splitLast"] = common.SplitLast
-	fm["splitFirst"] = common.SplitFirst
-	fm["trim"] = common.Trim
-	fm["trimPrefix"] = common.TrimPrefix
-	fm["trimSuffix"] = common.TrimSuffix
-	fm["replace"] = common.Replace
-	fm["contains"] = common.Contains
-	fm["indexOf"] = common.IndexOf
-	fm["int2word"] = common.IntToWordLower
-	fm["Int2Word"] = common.IntToWordTitle
-	fm["INT2WORD"] = common.IntToWordUpper
-	fm["plural"] = common.Pluralize
-	fm["abbreviate"] = helper.Abbreviate
-	fm["nl"] = common.NewLine
-	fm["toJson"] = common.ToJson
 
+	common.PopulateFuncMap(fm)
 	filtercpp.PopulateFuncMap(fm)
 	filtergo.PopulateFuncMap(fm)
 	filterts.PopulateFuncMap(fm)
