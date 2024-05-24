fa03ada1d39b6721aa2b29f57aa490a93618ebac
Wolfgang Bremer <wbremer@apigear.io>
2024-05-24 23:27:57 +0200

    refactor(cpp): support for extern types

--- pkg/gen/filters/filtercpp/extern.go
+++ pkg/gen/filters/filtercpp/extern.go
@@ -5,10 +5,14 @@ import (
 )
 
 type CppExtern struct {
-	NameSpace string
-	Include   string
-	Name      string
-	Library   string
+	NameSpace    string
+	Include      string
+	Name         string
+	Default      string
+	Component    string
+	Package      string
+	ConanPackage string
+	ConanVersion string
 }
 
 func parseCppExtern(schema *model.Schema) CppExtern {
@@ -19,15 +23,23 @@ func parseCppExtern(schema *model.Schema) CppExtern {
 func cppExtern(xe *model.Extern) CppExtern {
 	ns := xe.Meta.GetString("cpp.namespace")
 	inc := xe.Meta.GetString("cpp.include")
-	lib := xe.Meta.GetString("cpp.library")
 	name := xe.Meta.GetString("cpp.name")
+	dft := xe.Meta.GetString("cpp.default")
+	cmakePackage := xe.Meta.GetString("cpp.package")
+	cmakeComponent := xe.Meta.GetString("cpp.component")
+	conanPackage := xe.Meta.GetString("cpp.conanpackage")
+	conanVersion := xe.Meta.GetString("cpp.conanversion")
 	if name == "" {
 		name = xe.Name
 	}
 	return CppExtern{
-		NameSpace: ns,
-		Include:   inc,
-		Name:      name,
-		Library:   lib,
+		NameSpace:    ns,
+		Include:      inc,
+		Name:         name,
+		Default:      dft,
+		Package:      cmakePackage,
+		Component:    cmakeComponent,
+		ConanPackage: conanPackage,
+		ConanVersion: conanVersion,
 	}
 }
