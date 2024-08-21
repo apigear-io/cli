1f8d6d212c3f253a107b97fa3b2817b02c9259c9
Jürgen Ryannel <juergen@ryannel.org>
2024-08-21 09:32:20 +0200

    update template lint

--- pkg/cmd/tpl/lint.go
+++ pkg/cmd/tpl/lint.go
@@ -1,6 +1,10 @@
 package tpl
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/apigear-io/cli/pkg/gen"
+	"github.com/apigear-io/cli/pkg/model"
+	"github.com/spf13/cobra"
+)
 
 func NewLintCommand() *cobra.Command {
 	var dir string
@@ -8,9 +12,18 @@ func NewLintCommand() *cobra.Command {
 		Use:   "lint",
 		Short: "Lint a template",
 		Long:  `Lint a template`,
-		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Println("lint template")
+			_, err := gen.New(gen.Options{
+				TemplatesDir: dir,
+				System:       model.NewSystem("test"),
+				Features:     []string{"all"},
+				Force:        true,
+			})
+			if err != nil {
+				cmd.Printf("template dir '%s' is not valid: %s\n", dir, err)
+			} else {
+				cmd.Printf("template dir '%s' is valid\n", dir)
+			}
 		},
 	}
 	cmd.Flags().StringVarP(&dir, "dir", "d", ".", "template directory")
