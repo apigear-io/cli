8aed5998452d11ad14dfa01d6b5ad3e057841bbf
Jürgen Ryannel <juergen@ryannel.org>
2025-06-17 16:45:57 +0200

    Update demo.sim.js

--- pkg/vfs/demo.sim.js
+++ pkg/vfs/demo.sim.js
@@ -3,41 +3,25 @@ const service = $createService("demo.Counter", {
     count: 0,
 });
 
-service.onProperty("count", function (count) {
+service.$.onProperty("count", function (count) {
     console.log("count:", count);
 });
 
-service.setMethod("increment", function () {
+service.increment = function() {
     console.log("incremented");
-    const count = service.getProperty("count");
+    const count = service.count
     if (count >= 10) {
-        service.callMethod("reset");
+        service.reset()
     }
-    service.setProperty("count", count + 1);
-});
+    service.count++
+}
 
-service.setMethod("reset", function () {
-    service.setProperty("count", 0);
-    service.emitSignal("reset", service.getProperty("count"));
-})
-
-
-// create a simulation client to trigger our own service
-const client = $createClient("demo.Counter");
-
-client.onProperty("count", function (count) {
-    console.log("count:", count);
-});
-
-client.onSignal("reset", function (count) {
-    console.log("counter was reset to", count);
-});
-
+service.reset = function() {
+    service.count = 0
+    service.$.emitSignal("reset", service.getProperty("count"));
+}
 
 
 function main() {
-    client.setProperty("count", 1);
-    setInterval(() => {
-        client.callMethod("increment");
-    }, 1000);
+    service.increment()
 }
