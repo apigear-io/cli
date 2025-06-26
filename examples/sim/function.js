b74e0c2228b3ac8149a11bdb99b1eba2df887df4
Jürgen Ryannel <juergen@ryannel.org>
2025-06-26 15:59:01 +0200

    update examples

--- examples/sim/function.js
+++ examples/sim/function.js
@@ -1,3 +1,12 @@
+// Simple function example showing basic simulation script structure
 function main() {
     console.log("main called");
+    
+    // This example demonstrates that simulations can be simple functions
+    // without services if no state management is needed
+    
+    // Exit simulation
+    if (typeof $quit === 'function') {
+        $quit();
+    }
 }
