0f3409b51ccedf8fbba451ea894fc26ab5ceb6fc
Wolfgang Bremer <wbremer@apigear.io>
2025-07-24 13:50:31 +0200

    feat(mcp): add specificationSchema tool

--- pkg/cmd/spec/mcp.go
+++ pkg/cmd/spec/mcp.go
@@ -11,6 +11,7 @@ import (
 
 func RegisterMCPTools(s *server.MCPServer) {
 	registerSpecCheckTool(s)
+	registerSpecShemaTool(s)
 }
 
 func registerSpecCheckTool(s *server.MCPServer) {
@@ -23,7 +24,8 @@ func registerSpecCheckTool(s *server.MCPServer) {
 		if err != nil {
 			return mcp.NewToolResultError(err.Error()), nil
 		}
-		result := ""
+
+		var result string
 		spec, err := spec.CheckFile(file)
 		if err != nil {
 			return mcp.NewToolResultError(err.Error()), nil
@@ -38,5 +40,50 @@ func registerSpecCheckTool(s *server.MCPServer) {
 			}
 		}
 		return mcp.NewToolResultText(string(result)), nil
+	})
+}
+
+func registerSpecShemaTool(s *server.MCPServer) {
+	specCheckTool := mcp.NewTool("specificationSchema",
+		mcp.WithDescription("Show the schema for module, solution, rules documents in either yaml or json format"),
+		mcp.WithString("type", mcp.Required(), mcp.Description("Document type (module, solution, rules)")),
+		mcp.WithString("format", mcp.Required(), mcp.Description("Output schema format (yaml, json)")),
+	)
+	s.AddTool(specCheckTool, func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+		docType, err := request.RequireString("type")
+		if err != nil {
+			return mcp.NewToolResultError(err.Error()), nil
+		}
+		var documentType spec.DocumentType
+		switch docType {
+		case "module":
+			documentType = spec.DocumentTypeModule
+		case "solution":
+			documentType = spec.DocumentTypeSolution
+		case "rules":
+			documentType = spec.DocumentTypeRules
+		default:
+			return mcp.NewToolResultError(fmt.Sprintf("%s is not a valid document type", docType)), nil
+		}
+
+		outputformat, err := request.RequireString("format")
+		if err != nil {
+			return mcp.NewToolResultError(err.Error()), nil
+		}
+		var schemaFormat spec.SchemaFormat
+		switch outputformat {
+		case "yaml":
+			schemaFormat = spec.SchemaFormatYaml
+		case "json":
+			schemaFormat = spec.SchemaFormatJson
+		default:
+			return mcp.NewToolResultError(fmt.Sprintf("%s is not a valid output format", outputformat)), nil
+		}
+
+		schema, err := spec.ShowSchemaFile(documentType, schemaFormat)
+		if err != nil {
+			return mcp.NewToolResultError(err.Error()), nil
+		}
+		return mcp.NewToolResultText(fmt.Sprintf("%s\n", *schema)), nil
 	})
 }
