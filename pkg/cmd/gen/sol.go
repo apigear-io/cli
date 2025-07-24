b1e0068e3b5d359db92f1f1f531ed57903d2c2ad
Wolfgang Bremer <wbremer@apigear.io>
2025-07-24 16:36:41 +0200

    refactor(cli): extract solution generation logic

--- pkg/cmd/gen/sol.go
+++ pkg/cmd/gen/sol.go
@@ -28,41 +28,45 @@ as also the other options. To create a demo module or solution use the 'project 
 		RunE: func(cmd *cobra.Command, args []string) error {
 			log.Info().Msgf("generating solution %s", args[0])
 			source = args[0]
-			result, err := spec.CheckFileAndType(source, spec.DocumentTypeSolution)
-			if err != nil {
-				return err
-			}
-			if !result.Valid() {
-				for _, err := range result.Errors {
-					log.Warn().Msgf("source %s at %s error %s", source, err.Field, err.Description)
-				}
-				return nil
-			}
-			runner := sol.NewRunner()
-			runner.OnTask(func(evt *tasks.TaskEvent) {
-				log.Debug().Msgf("[%s] task %s: %v", evt.State, evt.Name, evt.Meta)
-			})
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-
-			if watch {
-				err := runner.WatchSource(ctx, source, force)
-				if err != nil {
-					log.Error().Err(err).Msg("watching solution file")
-					cancel()
-				}
-				helper.WaitForInterrupt(cancel)
-			} else {
-				err = runner.RunSource(ctx, source, force)
-				if err != nil {
-					return err
-				}
-			}
-			return nil
+			return runGenerateSolution(source, watch, exec, force)
 		},
 	}
 	cmd.Flags().BoolVarP(&watch, "watch", "", false, "watch solution file for changes")
 	cmd.Flags().StringVarP(&exec, "exec", "", "", "execute a command after generation")
 	cmd.Flags().BoolVarP(&force, "force", "", false, "force overwrite")
 	return cmd
+}
+
+func runGenerateSolution(solutionPath string, watch bool, _ string, force bool) error {
+	result, err := spec.CheckFileAndType(solutionPath, spec.DocumentTypeSolution)
+	if err != nil {
+		return err
+	}
+	if !result.Valid() {
+		for _, err := range result.Errors {
+			log.Warn().Msgf("source %s at %s error %s", solutionPath, err.Field, err.Description)
+		}
+		return nil
+	}
+	runner := sol.NewRunner()
+	runner.OnTask(func(evt *tasks.TaskEvent) {
+		log.Debug().Msgf("[%s] task %s: %v", evt.State, evt.Name, evt.Meta)
+	})
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if watch {
+		err := runner.WatchSource(ctx, solutionPath, force)
+		if err != nil {
+			log.Error().Err(err).Msg("watching solution file")
+			cancel()
+		}
+		helper.WaitForInterrupt(cancel)
+	} else {
+		err = runner.RunSource(ctx, solutionPath, force)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
