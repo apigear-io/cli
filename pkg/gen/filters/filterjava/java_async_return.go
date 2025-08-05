9b75984c00fe85f717cd0a5335fc12ddea84e1eb
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2025-08-05 11:23:00 +0200

    filters(java): add async version of return filter
    the completable future doesn not just wrap a type, it requires an object
    so instead of simple types we use object version type (e.g. Integer
    instead of int)
    The filter returns the completable future with the proper type

---
+++ pkg/gen/filters/filterjava/java_async_return.go
@@ -0,0 +1,76 @@
+package filterjava
+
+import (
+	"fmt"
+
+	"github.com/apigear-io/cli/pkg/model"
+)
+
+func ToAsyncReturnString(prefix string, schema *model.Schema) (string, error) {
+	if schema == nil {
+		return "xxx", fmt.Errorf("ToReturnString schema is nil")
+	}
+	var text string
+	switch schema.KindType {
+	case model.TypeString:
+		text = "String"
+	case model.TypeInt:
+		text = "Integer"
+	case model.TypeInt32:
+		text = "Integer"
+	case model.TypeInt64:
+		text = "Long"
+	case model.TypeFloat:
+		text = "Float"
+	case model.TypeFloat32:
+		text = "Float"
+	case model.TypeFloat64:
+		text = "Double"
+	case model.TypeBool:
+		text = "Boolean"
+	case model.TypeEnum:
+		symbol := schema.GetEnum()
+		text = fmt.Sprintf("%s%s", prefix, symbol.Name)
+	case model.TypeStruct:
+		symbol := schema.GetStruct()
+		text = fmt.Sprintf("%s%s", prefix, symbol.Name)
+	case model.TypeExtern:
+		xe := parseJavaExtern(schema)
+		text = fmt.Sprintf("%s%s", prefix, xe.Name)
+	case model.TypeInterface:
+		symbol := schema.GetInterface()
+		text = fmt.Sprintf("%s%s", prefix, symbol.Name)
+	case model.TypeVoid:
+		text = "void"
+	default:
+		return "xxx", fmt.Errorf("javaReturn unknown schema %s", schema.Dump())
+	}
+	if schema.IsArray {
+		switch schema.KindType {
+		case model.TypeInt:
+			text = "int"
+		case model.TypeInt32:
+			text = "int"
+		case model.TypeInt64:
+			text = "long"
+		case model.TypeFloat:
+			text = "float"
+		case model.TypeFloat32:
+			text = "float"
+		case model.TypeFloat64:
+			text = "double"
+		case model.TypeBool:
+			text = "boolean"
+		}
+		text = fmt.Sprintf("%s[]", text)
+	}
+	text = fmt.Sprintf("CompletableFuture<%s>", text)
+	return text, nil
+}
+
+func javaAsyncReturn(prefix string, node *model.TypedNode) (string, error) {
+	if node == nil {
+		return "xxx", fmt.Errorf("javaReturn node is nil")
+	}
+	return ToAsyncReturnString(prefix, &node.Schema)
+}
