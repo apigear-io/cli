e845cb0278481a3df056285285f17afca89c4f25
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2025-10-13 15:43:42 +0200

    feat(gen): prepare java filters for externs
    now generating file per an extern is possible in the rules file.
    This was required for java template, where each class needs separate
    files. Used for the android parcelable version of an extern.

--- pkg/gen/filters/filterjava/extern.go
+++ pkg/gen/filters/filterjava/extern.go
@@ -3,9 +3,11 @@ package filterjava
 import "github.com/apigear-io/cli/pkg/model"
 
 type JavaExtern struct {
-	Package string
-	Name    string
-	Default string
+	Package         string
+	Name            string
+	Default         string
+	Version         string
+	DownloadPackage string
 }
 
 func parseJavaExtern(schema *model.Schema) JavaExtern {
@@ -17,12 +19,17 @@ func javaExtern(xe *model.Extern) JavaExtern {
 	ns := xe.Meta.GetString("java.package")
 	name := xe.Meta.GetString("java.name")
 	dft := xe.Meta.GetString("java.default")
+	ver := xe.Meta.GetString("java.version")
+	dwnld_pck := xe.Meta.GetString("java.download_package")
+
 	if name == "" {
 		name = xe.Name
 	}
 	return JavaExtern{
-		Package: ns,
-		Name:    name,
-		Default: dft,
+		Package:         ns,
+		Name:            name,
+		Default:         dft,
+		Version:         ver,
+		DownloadPackage: dwnld_pck,
 	}
 }
